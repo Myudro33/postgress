31151c2289631a65df3d6258bb25500108e60721
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-06 11:33:22 +0400

    feat: enhance authentication middleware with role-based access control

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const auth = (req, res, next) => {
-  const token = req.headers.authorization;
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "unauthorized" });
   }
@@ -12,4 +15,34 @@ export const auth = (req, res, next) => {
     req.user = decoded;
     next();
   });
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user.role === "admin") {
+    return next();
+  }
+  return res.status(403).json({ message: "only admin can access this route" });
+};
+
+export const isCostumer = (req, res, next) => {
+  if (req.user.role === "admin") {
+    return next();
+  }
+  if (req.user.role !== "customer") {
+    return res
+      .status(403)
+      .json({ message: "only customer can access this route" });
+  }
+  next();
+};
+export const isManager = (req, res, next) => {
+  if (req.user.role === "admin") {
+    return next();
+  }
+  if (req.user.role !== "manager") {
+    return res
+      .status(403)
+      .json({ message: "only manager can access this route" });
+  }
+  next();
 };
