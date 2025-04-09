af9778c27edfa76746a066552561804bd8010991
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-09 19:00:50 +0400

    feat: implement email service using nodemailer for sending emails

---
+++ utils/emailService.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+export const sendMail = async (email, subject, html) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: subject,
+    html: html,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+    return true;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return false;
+  }
+};
