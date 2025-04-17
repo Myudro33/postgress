3c96f68fe6db8e10f6d9c45840eba9eb22a99f88
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-17 19:12:52 +0400

    feat: add static file serving for uploads directory

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import swaggerUI from "swagger-ui-express";
 import specs from "./models/swagger.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use("/uploads", express.static("./uploads"));
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.get("/", (req, res) => {
   res.json({ message: "welcome to postgress" });
